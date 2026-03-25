e52a6887ffe614329ee61a926c57ba32b8eaf41b
anandgupta42 <anand@altimate.ai>
2026-03-24 21:00:59 -0700

    refactor: simplify stg_orders query

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -7,15 +7,9 @@ with source as (
 renamed as (
 
     select
-        order_id,
-        user_id                             as customer_id,
-        status                              as order_status,
-        gender,
-        created_at                          as ordered_at,
-        returned_at,
-        shipped_at,
-        delivered_at,
-        num_of_item                         as item_count
+        
+        *
+    
     from source
 
 )
